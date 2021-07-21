d878890456788cdfefa26b4159a7b02d35ede997
Ifeoluwa Onigbinde <onigbinde.i@gmail.com>
2021-07-21 21:52:46 +0100

    Wrote code for bi-directional search

--- Nodes.py
+++ Nodes.py
@@ -1,8 +1,9 @@
 
 class Nodes:
-    def __init__(self, cordinates, left, right, value):
+    def __init__(self, cordinates, left, right, value, head):
         self.cordinates = cordinates
         self.left = left
         self.right = right
         self.value = value
+        self.head = head
 
